cfba49c730756ceda29163d00bb340b95e8371cf
arjan <arjan@ARRU-HI>
2024-05-23 17:37:45 +0200

    Test

--- VillageOfTesting/TestFile/VillageTests.cs
+++ VillageOfTesting/TestFile/VillageTests.cs
@@ -4,18 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VillageOfTesting;
+using VillageOfTesting.Objects;
+using VillageOfTesting.OccupationActions;
 
 namespace VillageOfTesting.TestFile
 {
     public class VillageTests
     {
         [TestFixture]
-        public class VillageTests
+        public class VillageTEsts
         {
             private Village village;
 
             [SetUp]
-            
+            public void SetUp()
+            {
+                village = new Village();
+            }
+
+            [Test]
+            public void Day_NoWorkers()
+            {
+                village.Food = 10;
+
+                village.Day();
+            }
         }
     }
 }
